5f6ff22bb3b6942f1f6f6faa60d12164a0ec46aa
alumno <alumno@E302>
2019-09-05 08:56:41 -0700

    55

--- Catalogo/Peliculas.cs
+++ Catalogo/Peliculas.cs
@@ -9,12 +9,12 @@ namespace Catalogo
     class Peliculas
     {
        public string Titulo { get; set; }
-       // public int Estreno { get; set; } //  1800< >2019
+       //public int Estreno { get; set; } //  1800< >2019
        public string Director { get; set; }
-       // public string Genero { get; set; }
+       public string Genero { get; set; }
        public string Pais { get; set; }
-       public int Minutos { get; set; }// #>0
-       public int Ratin { get; set; } //0 -10
+       //public int Minutos { get; set; }// #>0
+       //public int Rating { get; set; } //0 -10
        public string Sinopsis { get; set; }
 
         public List<Playlist> Lista { get; set; }
@@ -25,7 +25,7 @@ namespace Catalogo
         }
 
         private int _estreno { get; set; }
-        private int Estreno
+        public int Estreno
         {
             get
             {
@@ -52,7 +52,78 @@ namespace Catalogo
             }
         }
 
+        private int _minutos {get;set;}
+        public int  Minutos
+        {
+            get
+            {
+                return _minutos;
+            }
+            set
+            {
+                if (value>0)
+                {
+                    _minutos = value;
+                }
+                else 
+                {
+                    throw new Exception ("debe durar al menos un min");
+                }
+            }
+        }
 
+        private double _rating {get; set;}
+        public double Rating
+        {
+            get
+            {
+                return _rating;
+            }
+            set 
+            {
+                if (value>=0)
+                {
+                    if (value<10)
+                    {
+                        _rating = value;
+
+                    }
+                    else
+                    {
+                        throw new Exception("el rating no puede ser mayor a 10");
+                    }
+                }    
+                else 
+                {
+                    throw new Exception ("el rating no puede ser menor a 0");
+                }
+            }
+        }
+
+        public Peliculas ()
+        {
+            Titulo = "Sin definir";
+            Estreno = 2002;
+            Director="Sin definir";
+            Genero ="Sin definir";
+            Pais ="Sin definir";
+            Minutos=23;
+            Rating=5;
+            Sinopsis= "Sin definir";
+
+        }
+        public Peliculas (string titulo, int estreno, string director, string genero, string pais, int minutos, int rating, string sinopsis)
+        {
+            Titulo = titulo;
+            Estreno = estreno;
+            Director = director;
+            Genero = genero;
+            Pais = pais;
+            Minutos = minutos;
+            Rating = rating;
+            Sinopsis = sinopsis;
+        }
+        
 
     }
 }
--- Catalogo/Program.cs
+++ Catalogo/Program.cs
@@ -10,11 +10,48 @@ namespace Catalogo
     {
         static void Main(string[] args)
         {
-            List<Peliculas> peliculas1 = new List<Peliculas>();
+          
+            
+            
+            
+            /* List<Peliculas> peliculas1 = new List<Peliculas>();
 
-           
+            Peliculas primeras = new Peliculas();
+            primeras.Titulo = "Pulp Fiction";
+            primeras.Estreno = 1994;
+            primeras.Director ="Tarantino";
+            primeras.Genero= "Accion";
+            primeras.Pais = "USA";
+            primeras.Minutos = 153;
+            primeras.Rating = 8.5;
+            primeras.Sinopsis = "Jules y Vincent, dos asesinos a sueldo con no demasiadas luces, trabajan para el gángster Marsellus Wallace";
+            Peliculas.Add(primeras);
 
+            Playlist uno = new Playlist ("peliculas de accion", "las que mas me gustaron");
+            Playlist dos = new Playlist ("peliculas de terror", "las que mas me gustaron");
+            Playlist dos = new Playlist ("peliculas de terror", "las que mas me gustaron");
 
+            primeras.Lista.Add(playlist1);
+            primeras.Lista.Add(playlist2);
+
+            foreach (Peliculas peliculas in peliculas1)
+            {
+                Console.WriteLine("Titulo:"+ peliculas.Titulo);
+                Console.WriteLine("Estreno:"+ peliculas.Estreno);
+                Console.WriteLine("Director:"+ peliculas.Director);
+                Console.WriteLine("Genero:"+ peliculas.Genero);
+                Console.WriteLine("Pais:"+ peliculas.Pais);
+                Console.WriteLine("Minutos:"+ peliculas.Minutos);
+                Console.WriteLine("Rating:"+ peliculas.Rating);
+                Console.WriteLine("Sinpsis:"+ peliculas.Sinopsis);
+                foreach (Playlist playlist in peliculas.Lista)
+                {
+                    Console.WriteLine("*"+ playlist.Titulo + "*" + "*" + playlist.Descripcion);
+
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
         }
     }
 }
